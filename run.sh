0eb58fc8a5fd82fd1f639d064a6f8a5eae6dfe78
Youssef Boulaoaune <43298428+Boulaouaney@users.noreply.github.com>
2023-03-09 19:08:06 +0900

    create finetuning script

---
+++ run.sh
@@ -0,0 +1,35 @@
+python -m torch.distributed.launch \
+	--nproc_per_node 6 FineTuningScript.py \
+	--model_name_or_path="openai/whisper-large-v2" \
+	--dataset_name="Bingsu/zeroth-korean" \
+	--language="korean" \
+	--train_split_name="train" \
+	--eval_split_name="test" \
+	--model_index_name="Whisper Large v2 Korean" \
+	--max_steps="5000" \
+	--output_dir="./whisper-large-v2-Ko" \
+	--per_device_train_batch_size="32" \
+	--per_device_eval_batch_size="16" \
+	--logging_steps="25" \
+	--learning_rate="1e-5" \
+	--warmup_steps="500" \
+	--evaluation_strategy="steps" \
+	--eval_steps="1000" \
+	--save_strategy="steps" \
+	--save_steps="1000" \
+	--generation_max_length="225" \
+	--freeze_feature_encoder="False" \
+	--report_to="tensorboard" \
+	--metric_for_best_model="wer" \
+	--greater_is_better="False" \
+	--load_best_model_at_end \
+	--gradient_checkpointing \
+	--fp16 \
+	--overwrite_output_dir \
+	--do_train \
+	--do_eval \
+	--predict_with_generate \
+	--do_normalize_eval \
+	--streaming="False" \
+	--use_auth_token="True" \
+	--push_to_hub="True"
