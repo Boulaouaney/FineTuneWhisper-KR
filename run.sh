d168c17ddb1e4cc2f34e62df7a21ed587437fa0d
Youssef Boulaouane <y.boulaouane11@gmail.com>
2023-03-10 16:25:46 +0900

    update number of gpus

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 python3 -m torch.distributed.launch \
-	--nproc_per_node 6 --node_rank 1 FineTuningScript.py \
+	--nproc_per_node 7 --node_rank 1 FineTuningScript.py \
 	--model_name_or_path="openai/whisper-large-v2" \
 	--dataset_name="Bingsu/zeroth-korean" \
 	--language="korean" \
