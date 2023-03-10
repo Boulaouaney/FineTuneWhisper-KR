36d8ef29fb7f81ec1c17514e9869d715f5070af7
Youssef Boulaoaune <43298428+Boulaouaney@users.noreply.github.com>
2023-03-10 14:27:09 +0900

    Update FineTuningScript.py

--- FineTuningScript.py
+++ FineTuningScript.py
@@ -265,6 +265,8 @@ def main():
         model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
     else:
         model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+        # dirty fix
+        training_args.n_gpu = 6
 
     # Sending telemetry. Tracking the example usage helps us better allocate resources to maintain them. The
     # information sent is the one passed as arguments along with your Python/PyTorch versions.
