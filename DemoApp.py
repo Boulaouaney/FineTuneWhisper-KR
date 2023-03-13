a47559e87bfdd28eb7a0e523ae55eabaec59d894
Youssef Boulaoaune <43298428+Boulaouaney@users.noreply.github.com>
2023-03-13 13:54:45 +0900

    Update DemoApp.py live transcription

--- DemoApp.py
+++ DemoApp.py
@@ -5,12 +5,18 @@ pipe = pipeline(model="byoussef/whisper-large-v2-Ko")
 
 def transcribe(audio):
     text = pipe(audio)["text"]
-    return text
+    state += text + " "
+    return state, state
 def main():
     iface = gr.Interface(
         fn=transcribe,
-        inputs=gr.Audio(source="microphone", type="filepath"),
-        outputs="text",
+        inputs=[gr.Audio(source="microphone", type="filepath", streaming=True),
+        "state"],
+        outputs=[
+            "textbox",
+            "state"
+        ],
+        live=True
         title="Whisper Large-v2 Korean",
         description="Realtime demo for Korean speech recognition using a fine-tuned Whisper large-v2 model.",
     )
